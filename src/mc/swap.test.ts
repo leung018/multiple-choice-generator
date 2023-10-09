b85f25a6089ad685e284303884d64f79e399743c
Leung Cheng <monewckl@hotmail.com.hk>
2023-10-09 18:45:20 +0800

    Sync test name conventions

--- src/mc/swap.test.ts
+++ src/mc/swap.test.ts
@@ -2,14 +2,14 @@ import { MultipleChoiceSwapper } from './swap'
 import { MultipleChoice } from './mc'
 
 describe('MultipleChoiceSwapper.getSignificantlySwapped', () => {
-  it('compute swaps for two choices', () => {
+  it('should compute swaps for two choices', () => {
     const mc = new MultipleChoice(['a', 'b'], 0)
     expect(MultipleChoiceSwapper.getSignificantlySwapped(mc)).toEqual(
       new Set([new MultipleChoice(['b', 'a'], 1)]),
     )
   })
 
-  it('compute swaps for three choices', () => {
+  it('should compute swaps for three choices', () => {
     const mc = new MultipleChoice(['a', 'b', 'c'], 1)
     expect(MultipleChoiceSwapper.getSignificantlySwapped(mc)).toEqual(
       new Set([
