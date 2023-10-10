21b19d5abd126605f74a6a01bed7815c2907ae5d
Leung Cheng <monewckl@hotmail.com.hk>
2023-10-10 18:37:24 +0800

    Draft toThrowCustomError

---
+++ src/testutils/assert/check_error.test.ts
@@ -0,0 +1,33 @@
+import { CustomBaseError } from '../../utils/err'
+import { expect } from '@jest/globals'
+import './check_error'
+
+class TestError extends CustomBaseError {}
+
+describe('toThrowCustomError', () => {
+  it('should fail if it does not throw error', () => {
+    expect(() => {
+      expect(() => {}).toThrowCustomError(TestError)
+    }).toThrowError(
+      'Expected constructor: TestError\nReceived function did not throw',
+    )
+  })
+
+  it('should fails if it is not target exception', () => {
+    expect(() => {
+      expect(() => {
+        throw new Error('Not a TestError')
+      }).toThrowCustomError(TestError)
+    }).toThrowError(
+      'Expected constructor: TestError\nReceived constructor: Error',
+    )
+  })
+
+  it('should pass if it throws the target exception', () => {
+    expect(() => {
+      expect(() => {
+        throw new TestError('TEST_ERROR')
+      }).toThrowCustomError(TestError)
+    }).not.toThrowError()
+  })
+})
---
+++ src/testutils/assert/check_error.ts
@@ -0,0 +1,60 @@
+import { expect } from '@jest/globals'
+import type { MatcherFunction } from 'expect'
+import { CustomBaseError } from '../../utils/err'
+
+type Newable<T> = { new (...args: any[]): T }
+
+const toThrowCustomError: MatcherFunction<
+  [expectedErrorType: Newable<CustomBaseError>, expectedCode?: string]
+> = function (received, expectedErrorType, expectedCode) {
+  if (typeof received !== 'function') {
+    throw new Error('This should be type function!')
+  }
+  const expectedConstructor = expectedErrorType.name
+
+  let error: Error | undefined = undefined
+
+  try {
+    received()
+  } catch (e) {
+    if (e instanceof Error) {
+      error = e
+    }
+  }
+
+  if (error === undefined) {
+    return {
+      pass: false,
+      message: () =>
+        `Expected constructor: ${expectedConstructor}\nReceived function did not throw`,
+    }
+  }
+  if (!(error instanceof expectedErrorType)) {
+    return {
+      pass: false,
+      message: () => {
+        const receivedConstructor = error?.constructor.name
+
+        return `Expected constructor: ${expectedConstructor}\nReceived constructor: ${receivedConstructor}`
+      },
+    }
+  }
+
+  // TODO: check error code
+
+  return {
+    pass: true,
+    message: () => 'Expected not to throw {}',
+  }
+}
+
+expect.extend({ toThrowCustomError })
+
+declare module 'expect' {
+  interface Matchers<R> {
+    toThrowCustomError(
+      expectedErrorType: Newable<CustomBaseError>,
+      expectedCode?: string,
+    ): R
+  }
+}
