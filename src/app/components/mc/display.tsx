6a632cd1a840fb7b03973be87c1533c05a2ec03c
Leung Cheng <monewckl@hotmail.com.hk>
2023-10-18 19:28:24 +0800

    Add select one choice logic for each question

--- src/app/components/mc/display.tsx
+++ src/app/components/mc/display.tsx
@@ -1,22 +1,45 @@
+'use client'
+
+import { useState } from 'react'
 import { MultipleChoiceQuestion } from '../../../mc/question'
 
 interface MultipleChoicePageProps {
   questions: MultipleChoiceQuestion[]
 }
 
-// TODO: Noted that won't test the rendering of mc selector and submit button by now. Test that part later in the feature of submitting the answer is more meaningful.
+// TODO: Noted that won't test the rendering of submit button by now. Test that part later in the feature of submitting the answer is more meaningful.
 export default function MultipleChoicePage({
   questions,
 }: MultipleChoicePageProps) {
+  const [questionToCheckedChoiceMap, setCheckedChoice] = useState<
+    Map<number, number>
+  >(new Map<number, number>())
+
+  const handleChoiceChange = (questionIndex: number, choiceIndex: number) => {
+    const newMap = new Map<number, number>(questionToCheckedChoiceMap)
+    newMap.set(questionIndex, choiceIndex)
+    setCheckedChoice(newMap)
+  }
+
   return (
     <div className="p-4">
-      {questions.map((question, index) => (
-        <div key={index} className="mb-4">
+      {questions.map((question, questionIndex) => (
+        <div key={questionIndex} className="mb-4">
           <p className="font-bold whitespace-pre-line">{question.title}</p>
           <div className="ml-4">
-            {question.mc.choices.map((choice, index) => (
-              <label key={index} className="flex items-center mb-2">
-                <input type="radio" className="mr-2" />
+            {question.mc.choices.map((choice, choiceIndex) => (
+              <label key={choiceIndex} className="flex items-center mb-2">
+                <input
+                  type="radio"
+                  className="mr-2"
+                  checked={
+                    choiceIndex ===
+                    questionToCheckedChoiceMap.get(questionIndex)
+                  }
+                  onChange={() =>
+                    handleChoiceChange(questionIndex, choiceIndex)
+                  }
+                />
                 {choice}
               </label>
             ))}
