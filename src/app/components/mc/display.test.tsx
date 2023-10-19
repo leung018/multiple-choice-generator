c1744a899a9a26e8e098d40abd69ea098fd53a99
Leung Cheng <monewckl@hotmail.com.hk>
2023-10-19 17:24:04 +0800

    Add  wrapper

--- src/app/components/mc/display.test.tsx
+++ src/app/components/mc/display.test.tsx
@@ -9,49 +9,46 @@ import MultipleChoicePage from './display'
 
 describe('MultipleChoicePage', () => {
   it('should render title and choices of a question', () => {
-    const question: MultipleChoiceQuestion = {
-      title: 'Sample Question?',
-      mc: MultipleChoice.createTestInstance({
-        choices: ['Answer 1', 'Answer 2'],
-      }),
-    }
-    const { getByText, getByLabelText } = render(
-      <MultipleChoicePage questions={[question]} />,
-    )
+    const { getByText, getByLabelText } = renderMultipleChoicePage({
+      questions: [
+        {
+          title: 'Sample Question?',
+          mc: MultipleChoice.createTestInstance({
+            choices: ['Answer 1', 'Answer 2'],
+          }),
+        },
+      ],
+    })
     expect(getByText('Sample Question?')).toBeInTheDocument()
     expect(getByLabelText('Answer 1')).toBeInTheDocument()
     expect(getByLabelText('Answer 2')).toBeInTheDocument()
   })
 
   it('should render multiple questions', () => {
-    const { getByText } = render(
-      <MultipleChoicePage
-        questions={[
-          MultipleChoiceQuestionFactory.createTestInstance({
-            title: 'Question 1',
-          }),
-          MultipleChoiceQuestionFactory.createTestInstance({
-            title: 'Question 2',
-          }),
-        ]}
-      />,
-    )
+    const { getByText } = renderMultipleChoicePage({
+      questions: [
+        MultipleChoiceQuestionFactory.createTestInstance({
+          title: 'Question 1',
+        }),
+        MultipleChoiceQuestionFactory.createTestInstance({
+          title: 'Question 2',
+        }),
+      ],
+    })
     expect(getByText('Question 1')).toBeInTheDocument()
     expect(getByText('Question 2')).toBeInTheDocument()
   })
 
   it('should select one choice in a question will uncheck other selected', () => {
-    const { getByLabelText } = render(
-      <MultipleChoicePage
-        questions={[
-          MultipleChoiceQuestionFactory.createTestInstance({
-            mc: MultipleChoice.createTestInstance({
-              choices: ['Choice 1', 'Choice 2'],
-            }),
+    const { getByLabelText } = renderMultipleChoicePage({
+      questions: [
+        MultipleChoiceQuestionFactory.createTestInstance({
+          mc: MultipleChoice.createTestInstance({
+            choices: ['Choice 1', 'Choice 2'],
           }),
-        ]}
-      />,
-    )
+        }),
+      ],
+    })
     const choice1 = getByLabelText('Choice 1')
     const choice2 = getByLabelText('Choice 2')
 
@@ -64,22 +61,20 @@ describe('MultipleChoicePage', () => {
   })
 
   it("should select one choice in a question won't affect other questions", () => {
-    const { getByLabelText } = render(
-      <MultipleChoicePage
-        questions={[
-          MultipleChoiceQuestionFactory.createTestInstance({
-            mc: MultipleChoice.createTestInstance({
-              choices: ['Question 1 Choice A', 'Question 1 Choice B'],
-            }),
+    const { getByLabelText } = renderMultipleChoicePage({
+      questions: [
+        MultipleChoiceQuestionFactory.createTestInstance({
+          mc: MultipleChoice.createTestInstance({
+            choices: ['Question 1 Choice A', 'Question 1 Choice B'],
           }),
-          MultipleChoiceQuestionFactory.createTestInstance({
-            mc: MultipleChoice.createTestInstance({
-              choices: ['Question 2 Choice A', 'Question 2 Choice B'],
-            }),
+        }),
+        MultipleChoiceQuestionFactory.createTestInstance({
+          mc: MultipleChoice.createTestInstance({
+            choices: ['Question 2 Choice A', 'Question 2 Choice B'],
           }),
-        ]}
-      />,
-    )
+        }),
+      ],
+    })
 
     const question1ChoiceA = getByLabelText('Question 1 Choice A')
     const question2ChoiceB = getByLabelText('Question 2 Choice B')
@@ -92,17 +87,15 @@ describe('MultipleChoicePage', () => {
   })
 
   it('should are choices unchecked at the beginning', () => {
-    const { getByLabelText } = render(
-      <MultipleChoicePage
-        questions={[
-          MultipleChoiceQuestionFactory.createTestInstance({
-            mc: MultipleChoice.createTestInstance({
-              choices: ['Choice 1', 'Choice 2'],
-            }),
+    const { getByLabelText } = renderMultipleChoicePage({
+      questions: [
+        MultipleChoiceQuestionFactory.createTestInstance({
+          mc: MultipleChoice.createTestInstance({
+            choices: ['Choice 1', 'Choice 2'],
           }),
-        ]}
-      />,
-    )
+        }),
+      ],
+    })
     const choice1 = getByLabelText('Choice 1')
     const choice2 = getByLabelText('Choice 2')
 
@@ -110,3 +103,14 @@ describe('MultipleChoicePage', () => {
     expect(choice2).not.toBeChecked()
   })
 })
+
+/* This wrapper function exist because in future, may need to move this test to other layer and the setup for it will be different too.
+ * So, with this wrapper function, it will be easier to change in the future.
+ */
+function renderMultipleChoicePage({
+  questions,
+}: {
+  questions: MultipleChoiceQuestion[]
+}) {
+  return render(<MultipleChoicePage questions={questions} />)
+}
