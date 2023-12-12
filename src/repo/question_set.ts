86598398250ffbdfcb9c2bd4c5b7774f7b545e29
Leung Cheng <monewckl@hotmail.com.hk>
2023-12-12 18:50:37 +0800

    Introduce QuestionSetSaveError

--- src/repo/question_set.ts
+++ src/repo/question_set.ts
@@ -1,8 +1,19 @@
 import { QuestionSet } from '../model/question_set'
+import { CustomBaseError } from '../utils/err'
 
 export interface QuestionSetRepo {
+  /**
+   * @throws {QuestionSetSaveError}
+   */
   save(questionSet: QuestionSet): void
   getQuestionSetByName(questionSetName: string): QuestionSet
+}
+
+type QuestionSetSaveErrorCode = 'DUPLICATE_QUESTION_SET_NAME'
+export class QuestionSetSaveError extends CustomBaseError {
+  constructor(code: QuestionSetSaveErrorCode, message?: string) {
+    super(code, message)
+  }
 }
 
 export class QuestionSetRepoFactory {
@@ -15,6 +26,12 @@ class InMemoryQuestionSetRepo implements QuestionSetRepo {
   private nameToQuestionSet: { [name: string]: QuestionSet } = {}
 
   save(questionSet: QuestionSet): void {
+    if (this.nameToQuestionSet[questionSet.name]) {
+      throw new QuestionSetSaveError(
+        'DUPLICATE_QUESTION_SET_NAME',
+        `QuestionSet with name ${questionSet.name} already exists`,
+      )
+    }
     this.nameToQuestionSet[questionSet.name] = questionSet
   }
 
