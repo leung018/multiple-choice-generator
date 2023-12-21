323b503d6d7a378a55e603e144996dfea471abb5
Leung Cheng <monewckl@hotmail.com.hk>
2023-12-21 22:41:37 +0800

    Test of not found

--- src/repo/question_set.test.ts
+++ src/repo/question_set.test.ts
@@ -1,6 +1,12 @@
 import { MultipleChoiceBuilder } from '../model/mc'
 import { QuestionSet, QuestionSetBuilderForTest } from '../model/question_set'
-import { LocalStorageQuestionSetRepo, QuestionSetRepo } from './question_set'
+import {
+  LocalStorageQuestionSetRepo,
+  QuestionSetGetError,
+  QuestionSetRepo,
+} from './question_set'
+import { expect } from '@jest/globals'
+import '../test_utils/assert/check_error'
 
 describe('QuestionSetRepo', () => {
   let repo: QuestionSetRepo
@@ -22,5 +28,12 @@ describe('QuestionSetRepo', () => {
   it('should add question set and get question set by name', () => {
     repo.addQuestionSet(questionSet)
     expect(repo.getQuestionSetByName(questionSet.name)).toEqual(questionSet)
+  })
+
+  it("should throw error when question set name doesn't exist", () => {
+    expect(() => repo.getQuestionSetByName('unknown')).toThrowCustomError(
+      QuestionSetGetError,
+      'QUESTION_SET_NOT_FOUND',
+    )
   })
 })
